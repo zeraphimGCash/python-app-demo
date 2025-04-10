57d801857c569565b999278da42ff5d6d7ff15bd
John christian Diamante <john.diamante@gcash.com>
2025-04-10 09:04:18 +0800

    Update app.py

---
+++ src/app.py
@@ -0,0 +1,19 @@
+# Endpoints
+# `/api/v1/details`
+# `/api/v1/healthz`
+
+# Link to Code
+# https://www.geeksforgeeks.org/flask-creating-first-simple-application/
+
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/api/v1/details')
+def details():
+    return 'Hello World'
+
+if __name__ == '__main__':
+    app.run()
+
